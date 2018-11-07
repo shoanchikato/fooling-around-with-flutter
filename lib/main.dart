6aa0343f9ae22ca8fab29cc51ad5e6951ece7d1e
shoan <sjchikato@gmail.com>
2018-11-07 17:45:03 +0200

    E9: JSON Storage (changes JSON - json, Map string string - string dynamic)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:async';
+import 'dart:io';
 import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(
@@ -17,36 +17,84 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  List data;
+  TextEditingController keyInputController = TextEditingController();
+  TextEditingController valueInputController = TextEditingController();
 
-  Future<String> getData() async {
-    http.Response response = await http.get(
-        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
-        headers: {"Accept": "application/json"});
-
-    this.setState(() => data = JSON.decode(response.body));
-
-    print(data[1]["title"]);
-    return "Success!";
-  }
+  File jsonFile;
+  Directory dir;
+  String fileName = "myJSONFile.json";
+  bool fileExists = false;
+  Map<String, dynamic> fileContent;
 
   @override
   void initState() {
-    this.getData();
+    super.initState();
+    getApplicationDocumentsDirectory().then((Directory directory) {
+      dir = directory;
+      jsonFile = new File(dir.path + "/" + fileName);
+      fileExists = jsonFile.existsSync();
+      if (fileExists) this.setState(() => fileContent = json.decode(jsonFile.readAsStringSync()));
+    });
+  }
+
+  @override
+  void dispose() {
+    keyInputController.dispose();
+    valueInputController.dispose();
+    super.dispose();
+  }
+
+  void createFile(Map<String, dynamic> content, Directory dir, String fileName) {
+    print("Creating File!");
+    File file = File(dir.path + "/" + fileName);
+    file.createSync();
+    fileExists = true;
+    file.writeAsStringSync(json.encode(content));
+  }
+
+  void writeToFile (String key, String value) {
+    print("Writing to file!");
+    Map<String, dynamic> content = {key: value};
+    if (fileExists) {
+      print("File exits"); 
+      Map<String, dynamic> jsonFileContent = json.decode(jsonFile.readAsStringSync());
+      jsonFileContent.addAll(content);
+      jsonFile.writeAsStringSync(json.encode(jsonFileContent));
+    } else {
+      print("File doesn't exist");
+      createFile(content, dir, fileName);
+    }
+    this.setState(() => fileContent = json.decode(jsonFile.readAsStringSync()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ListView"),
+        title: Text("JSON Tutorial"),
       ),
-      body: ListView.builder(
-          itemCount: data == null ? 0 : data.length,
-          itemBuilder: (BuildContext context, int index) => Card(
-              child: Text(data[index]["title"]),
-            ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 10.0),
           ),
+          Text("File content: ", style: TextStyle(fontWeight: FontWeight.bold)),
+          Text(fileContent.toString()),
+          Padding(padding: EdgeInsets.only(top: 10.0)),
+          Text("Add to JSON file: "),
+          TextField(
+            controller: keyInputController,
+          ),
+          TextField(
+            controller: valueInputController,
+          ),
+          Padding(padding: EdgeInsets.only(top: 20.0)),
+          RaisedButton(
+            child: Text("Add key, value pair"),
+            onPressed: () => writeToFile(keyInputController.text, valueInputController.text),
+          )
+        ],
+      ),
     );
   }
 }
