0c659afffd63a6d95f0ca6e3e66293daee97aa49
shoan <sjchikato@gmail.com>
2018-11-09 15:13:27 +0200

    Using the Bloc Pattern to Build Reactive Application with Streams

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,74 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'dart:convert';
-import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: HomePage(),
-    ),
-  );
-}
+import './bloc.dart';
+import './model.dart';
+import './provider.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  HomePageState createState() => HomePageState();
-}
+void main() => runApp(MyApp());
 
-class HomePageState extends State<HomePage> {
-  TextEditingController keyInputController = TextEditingController();
-  TextEditingController valueInputController = TextEditingController();
-
-  File jsonFile;
-  Directory dir;
-  String fileName = "myJSONFile.json";
-  bool fileExists = false;
-  Map<String, dynamic> fileContent;
-
-  @override
-  void initState() {
-    super.initState();
-    getApplicationDocumentsDirectory().then((Directory directory) {
-      dir = directory;
-      jsonFile = new File(dir.path + "/" + fileName);
-      fileExists = jsonFile.existsSync();
-      if (fileExists) this.setState(() => fileContent = json.decode(jsonFile.readAsStringSync()));
-    });
-  }
-
-  @override
-  void dispose() {
-    keyInputController.dispose();
-    valueInputController.dispose();
-    super.dispose();
-  }
-
-  void createFile(Map<String, dynamic> content, Directory dir, String fileName) {
-    print("Creating File!");
-    File file = File(dir.path + "/" + fileName);
-    file.createSync();
-    fileExists = true;
-    file.writeAsStringSync(json.encode(content));
-  }
-
-  void writeToFile (String key, String value) {
-    print("Writing to file!");
-    Map<String, dynamic> content = {key: value};
-    if (fileExists) {
-      print("File exits"); 
-      Map<String, dynamic> jsonFileContent = json.decode(jsonFile.readAsStringSync());
-      jsonFileContent.addAll(content);
-      jsonFile.writeAsStringSync(json.encode(jsonFileContent));
-    } else {
-      print("File doesn't exist");
-      createFile(content, dir, fileName);
-    }
-    this.setState(() => fileContent = json.decode(jsonFile.readAsStringSync()));
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MovieProvider(
+      movieBloc: MovieBloc(API()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final movieBloc = MovieProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("JSON Tutorial"),
+        title: Text('Bloc Example'),
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 10.0),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              onChanged: movieBloc.query.add,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Search for a Movie',
+              ),
+            ),
           ),
-          Text("File content: ", style: TextStyle(fontWeight: FontWeight.bold)),
-          Text(fileContent.toString()),
-          Padding(padding: EdgeInsets.only(top: 10.0)),
-          Text("Add to JSON file: "),
-          TextField(
-            controller: keyInputController,
+          StreamBuilder(
+            stream: movieBloc.log,
+            builder: (context, snapshot) => Container(
+                  child: Text(snapshot?.data ?? ''),
+                ),
           ),
-          TextField(
-            controller: valueInputController,
-          ),
-          Padding(padding: EdgeInsets.only(top: 20.0)),
-          RaisedButton(
-            child: Text("Add key, value pair"),
-            onPressed: () => writeToFile(keyInputController.text, valueInputController.text),
+          Flexible(
+            child: StreamBuilder(
+              stream: movieBloc.results,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) => ListTile(
+                        leading: CircleAvatar(
+                          child: Image.network(
+                              "https://image.tmdb.org/t/p/w92" +
+                                  snapshot.data[index].posterPath),
+                        ),
+                        title: Text(snapshot.data[index].title),
+                        subtitle: Text(snapshot.data[index].overview),
+                      ),
+                );
+              },
+            ),
           )
         ],
       ),
