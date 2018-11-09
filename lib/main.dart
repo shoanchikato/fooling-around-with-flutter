0713743849a9807b54c07b0417411dbcd8607b99
shoan <sjchikato@gmail.com>
2018-11-09 16:40:00 +0200

    Using the Different Style Widgets and Properties in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,56 @@
 import 'package:flutter/material.dart';
 
-import './bloc.dart';
-import './model.dart';
-import './provider.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MovieProvider(
-      movieBloc: MovieBloc(API()),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final movieBloc = MovieProvider.of(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bloc Example'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-              onChanged: movieBloc.query.add,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Search for a Movie',
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Transform(
+              transform: Matrix4.identity(),
+              child: Container(
+                width: 280.0,
+                height: 280.0,
+                decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                    colors: <Color>[
+                      Color(0xffef5350),
+                      Color(0x00ef5350),
+                    ],
+                  ),
+                  // boxShadow: <BoxShadow>[
+                  //   BoxShadow(
+                  //     color: Color(0xcc000000),
+                  //     offset: Offset(0.0, 2.0),
+                  //     blurRadius: 30.0,
+                  //   ),
+                  //   BoxShadow(
+                  //     color: Color(0x80000000),
+                  //     offset: Offset(5.0, 6.0),
+                  //     blurRadius: 2.0,
+                  //   ),
+                  // ],
+                  borderRadius: BorderRadius.circular(1000.0),
+                  border: Border.all(color: Colors.black),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  "Styling Stuff",
+                  style: TextStyle(
+                    fontSize: 42.0,
+                    fontWeight: FontWeight.w100,
+                    fontFamily: "Roboto",
+                    fontStyle: FontStyle.italic,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
           ),
-          StreamBuilder(
-            stream: movieBloc.log,
-            builder: (context, snapshot) => Container(
-                  child: Text(snapshot?.data ?? ''),
-                ),
-          ),
-          Flexible(
-            child: StreamBuilder(
-              stream: movieBloc.results,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData)
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) => ListTile(
-                        leading: CircleAvatar(
-                          child: Image.network(
-                              "https://image.tmdb.org/t/p/w92" +
-                                  snapshot.data[index].posterPath),
-                        ),
-                        title: Text(snapshot.data[index].title),
-                        subtitle: Text(snapshot.data[index].overview),
-                      ),
-                );
-              },
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
